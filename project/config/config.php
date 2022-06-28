7f98e80fcbeb8c0108b2c154ea8e23e66f7dc37e
NHuy <Nguyen.Huy@intertecsys.com>
2022-06-28 21:43:26 +0700

    project

---
+++ project/config/config.php
@@ -0,0 +1,13 @@
+<?php
+    define("PUBLIC_PATH", dirname(__FILE__) . "../public");
+
+    const DEVELOPMENT_ENVIRONMENT = 'true';
+    //db information
+    const DB_NAME = 'web_programming';
+    const DB_USER = 'root';
+    const DB_PASSWORD = 'hoang1901';
+    const DB_HOST = 'localhost';
+    // base path of the project
+    const BASE_PATH = 'http://localhost/project';
+
+    const PAGINATE_LIMIT = '5';
