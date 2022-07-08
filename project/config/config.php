93264b8a6577d5d394641103a9623bbfcb106ecb
Macbook Pro <macbookpro@MacBook-Pro-cua-Macbook-2.local>
2022-07-08 11:08:26 +0700

    addConfig

--- project/config/config.php
+++ project/config/config.php
@@ -3,11 +3,11 @@
 
     const DEVELOPMENT_ENVIRONMENT = 'true';
     //db information
-    const DB_NAME = 'mydatabase';
+    const DB_NAME = 'webprogram';
     const DB_USER = 'root';
     const DB_PASSWORD = 'root';
     const DB_HOST = 'localhost';
     // base path of the project
-    const BASE_PATH = 'http://localhost:8888/project2';
+    const BASE_PATH = 'http://localhost:8888/Webprogramming-Group11/project';
 
     const PAGINATE_LIMIT = '5';
